df511e46be58c011d28d988864ea9624fbf4c089
Blaten7 <162647372+Blaten7@users.noreply.github.com>
2024-12-11 11:04:48 +0900

    Init

---
+++ src/main/java/com/sparta/corporatetask/CorporateTaskApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.corporatetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorporateTaskApplication.class, args);
+    }
+
+}
