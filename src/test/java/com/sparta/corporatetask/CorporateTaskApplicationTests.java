d2f86a2284932d6ccbeabea5e8e097ac5ad6edb3
Blaten7 <162647372+Blaten7@users.noreply.github.com>
2024-12-11 10:58:17 +0900

    Init

---
+++ src/test/java/com/sparta/corporatetask/CorporateTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.corporatetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorporateTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
